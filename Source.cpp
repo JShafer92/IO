4af3c382513ce2e8a6a63e473d6ede0e8da6ab81
JShafer92 <ptwraith1@gmail.com>
2019-09-12 13:30:54 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "\n\t\t*** Welcome to a great day! ***\n";
+
+	cout << "Please enter your name.";
+	//creating a varianle of the data type string
+	string userName;
+	//gets input from console. get line grabs whole line, cin in only grabs first word.
+	getline(cin, userName);
+	//greet the user
+	cout << "\nHello" << userName << "welcome to Jumanji";
+
+	system("pause");
+
+	return 0;
+}
